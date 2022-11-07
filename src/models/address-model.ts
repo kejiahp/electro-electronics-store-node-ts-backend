82374dec4f0296582ecac9958733bd37bcfe3a77
kejiahp <popoolakejiah@gmail.com>
2022-11-07 17:53:37 +0100

    feat: models added

---
+++ src/models/address-model.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import { UserDocument } from "./user-model"
+
+export interface AddressDocument extends mongoose.Document{
+    userId: UserDocument["_id"],
+    state: string,
+    city: string,
+    country: string,
+    addressDesc: string,
+    phoneNumber1: number,
+    phoneNumber2: number,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+const AddressSchema = new mongoose.Schema<AddressDocument>({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    state: {
+        type: String,
+        required: [true, 'state is required']
+    },
+    city: {
+        type: String,
+        required: [true, 'city is required']
+    },
+    addressDesc: {
+        type: String,
+        required: [true, 'provide more address information']
+    },
+    country: {
+        type: String,
+        required: [true, 'country is required']
+    },
+    phoneNumber1: {
+        type: Number,
+        required: [true, 'phonenumber1 must be provided']
+    },
+    phoneNumber2: {
+        type: Number,
+        required: false
+    }
+}, {timestamps: true})
+
+const Address = mongoose.model("Address",AddressSchema)
+
+export default Address
